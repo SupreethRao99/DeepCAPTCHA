3c9da4e6f6cc9c953bdf25611b912d0253dcde3a
SupreethRao99 <55043035+SupreethRao99@users.noreply.github.com>
2021-04-02 11:57:16 +0530

    Updated ImagePreprocessing.py

--- ImagePreprocessing.py
+++ ImagePreprocessing.py
@@ -1,19 +1,22 @@
 import sys
 import cv2 as cv
 
-image = cv.imread('index.png')
+white = [255, 255, 255]
+green = [0, 255, 0]
+image = cv.imread('SampleImages/sample-003.png')
 
 if image is None:
     sys.exit('Could not read the image')
 
 img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+padded = cv.copyMakeBorder(thresh, 5, 5, 5, 5, cv.BORDER_CONSTANT, value=white)
 
-contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+contours, hierarchy = cv.findContours(padded, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 for cnt in contours:
     x, y, w, h = cv.boundingRect(cnt)
-    if cv.contourArea(cnt, True) > 64:  # heuristically determined value
-        cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    if cv.contourArea(cnt, True) > 50:  # heuristically determined value
+        cv.rectangle(padded, (x, y), (x + w, y + h), color=green, thickness=1)
 
-cv.imshow("Display Window", image)
+cv.imshow("Display Window", padded)
 k = cv.waitKey(0)
