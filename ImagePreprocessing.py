f85ed32f534d12d6aa3113de8c5a40393a93a365
SupreethRao99 <55043035+SupreethRao99@users.noreply.github.com>
2021-04-02 09:03:24 +0530

    Updated ImagePreprocessing.py

--- ImagePreprocessing.py
+++ ImagePreprocessing.py
@@ -1,19 +1,19 @@
 import sys
-
 import cv2 as cv
 
-image = cv.imread('test1.png')
-img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+image = cv.imread('index.png')
 
-if img is None:
+if image is None:
     sys.exit('Could not read the image')
 
+img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
-contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 for cnt in contours:
     x, y, w, h = cv.boundingRect(cnt)
-    cv.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 1)
-print(hierarchy)
+    if cv.contourArea(cnt, True) > 64:  # heuristically determined value
+        cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
 cv.imshow("Display Window", image)
 k = cv.waitKey(0)
