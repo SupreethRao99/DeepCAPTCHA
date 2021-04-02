6ad93fbf5310d58568e7931a6e6d3e3fa1472a41
SupreethRao99 <55043035+SupreethRao99@users.noreply.github.com>
2021-04-02 13:49:40 +0530

    Updated ImagePreprocessing.py

--- ImagePreprocessing.py
+++ ImagePreprocessing.py
@@ -9,10 +9,12 @@ if image is None:
     sys.exit('Could not read the image')
 
 img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
 ret, thresh = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 padded = cv.copyMakeBorder(thresh, 5, 5, 5, 5, cv.BORDER_CONSTANT, value=white)
 
 contours, hierarchy = cv.findContours(padded, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+
 for cnt in contours:
     x, y, w, h = cv.boundingRect(cnt)
     if cv.contourArea(cnt, True) > 50:  # heuristically determined value
