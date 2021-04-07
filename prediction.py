137d5a91081336ac82bf739e5b2a004354634aab
SupreethRao99 <55043035+SupreethRao99@users.noreply.github.com>
2021-04-07 17:56:57 +0530

    modified prediction.py

--- prediction.py
+++ prediction.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
+import cv2 as cv
+from resize import resize_to_fit
+import numpy as np
 
+image = cv.imread('extracted_letter_images/001.png')
+image_resized = resize_to_fit(image, 32, 32)
+image_resized = np.expand_dims(image_resized, axis=2)
 model = tf.keras.models.load_model('Model/CAPTCHA-Model')
-model.summary()
+model.predict(image_resized)
